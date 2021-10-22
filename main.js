5f21897dfd2cc931696732556a98fe3a814f931c
Nilton Pegas <92957971+niltonPegas@users.noreply.github.com>
2021-10-21 22:51:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const linksSocialMedia = {
+  github: "niltonPegas",
+  youtube: "sem_usuario",
+  facebook: "sem_usuario",
+  instagram: "nilton_pegas",
+  twitter: "verificar"
+}
+/* vírgulas para serparar os elementos dentro dos objetos */
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute("class")
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userGitHubLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+  /* Arrow function: É uma contração da escrita das funções em javascript
+  'function abc(){//code}' pode ser contraída para 'abc => //code' 
+    Este recurso será utilizado em '.then()'*/
+  /* 'fetch()' está pegando um resultado, mas ele não sabe que é um json. '.then()' está transformando a resposta em json */ 
+}
+getGitHubProfileInfos()
